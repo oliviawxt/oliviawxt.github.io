e17fe9d5ceeedbd10f0d10b4e2667d63b0b1fb69
OliviaWXT <do2467@126.com>
2019-10-26 23:50:47 +0800

    update

---
+++ assignment6/script.js
@@ -0,0 +1,69 @@
+$(document).ready(function() {
+
+    $("button#hide_h2").click(function() {
+        $("h2").hide(500);
+    });
+
+    $("button#show_h2").click(function() {
+        $("h2").show(300);
+        $("h2").css("color", "blue");
+        $("h2").html("You clicked me hard.");
+    });
+
+    $("button#clear_screen").click(function() {
+        var $x = $("container");
+        $x.empty();
+    });
+
+    $("button#get_data").click(function() {
+        var items = [];
+        var i = 0;
+        var airtable_read_endpoint = "https://api.airtable.com/v0/appY3zI6OL8r44Mib/%E9%9F%B3%E4%B9%90%E5%89%A7%E5%89%A7%E7%9B%AE?api_key=keyN5xX0FF8RiOrHI";
+        var dataSet = [];
+        $.getJSON(airtable_read_endpoint, function(result) {
+            $.each(result.records, function(key, value) {
+                items = [];
+                items.push(value.fields.name);
+                items.push(value.fields.location);
+                items.push(value.fields.province);
+                items.push(value.fields.theatre);
+                items.push(value.fields.date);
+                items.push(value.fields.type);
+                dataSet.push(items);
+                console.log(items);
+            }); // end .each
+            console.log(dataSet);
+
+            $('#table1').DataTable({
+                data: dataSet,
+                retrieve: true,
+                columns: [{
+                        title: "Name",
+                        defaultContent: ""
+                    },
+                    {
+                        title: "Location",
+                        defaultContent: ""
+                    },
+                    {
+                        title: "Province",
+                        defaultContent: ""
+                    },
+                    {
+                        title: "Theatre",
+                        defaultContent: ""
+                    },
+                    {
+                        title: "Date",
+                        defaultContent: ""
+                    },
+                    {
+                        title: "Type",
+                        defaultContent: ""
+                    },
+                ]
+            });
+        }); // end .getJSON
+    }); // end button
+
+}); // document ready
