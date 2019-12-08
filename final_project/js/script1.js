54c3d0671f4dfb0fc5c51335ce93a08169c8e701
OliviaWXT <do2467@126.com>
2019-12-08 17:01:52 +0800

    update

--- final_project/js/script1.js
+++ final_project/js/script1.js
@@ -15,5 +15,12 @@ $(document).ready(function() {
     $("a#analysis").click(function() {
         $("#ana").slideToggle(300);
     });
+    $("button#show_sites").click(function() {
+        $("#gov_sites").slideToggle(300);
+    });
+
+    $("button#get_location").click(function() {
+        $("#data_map").slideToggle(300);
+    });
 
 });
